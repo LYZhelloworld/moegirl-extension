13ce4ea3c35cfd723ded6c980b73f44bf9baec9a
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2021-01-07 16:44:00 +0800

    Add option pages

--- options/index.js
+++ options/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 let optionHideComments = document.getElementById('option-hide-comments');
-optionHideComments.addEventListener('change', () => {
-    chrome.storage.sync.set({ 'hide-comments': this.checked });
+optionHideComments.addEventListener('click', () => {
+    chrome.storage.sync.set({ 'hide-comments': optionHideComments.checked });
 });
 chrome.storage.sync.get({ 'hide-comments': false }, (data) => {
     optionHideComments.checked = data['hide-comments'];
