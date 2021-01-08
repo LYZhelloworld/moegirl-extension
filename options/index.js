a0a78dc111a386f479b68f6b2ae74a5c46e36a27
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2021-01-08 10:40:38 +0800

    Add bootstrap

--- options/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-let optionHideComments = document.getElementById('option-hide-comments');
-optionHideComments.addEventListener('click', () => {
-    chrome.storage.sync.set({ 'hide-comments': optionHideComments.checked });
-});
-chrome.storage.sync.get({ 'hide-comments': false }, (data) => {
-    optionHideComments.checked = data['hide-comments'];
-});
---
+++ popup/index.js
@@ -0,0 +1,10 @@
+'use strict';
+{
+    let optionHideComments = document.getElementById('option-hide-comments');
+    optionHideComments.addEventListener('click', () => {
+        chrome.storage.sync.set({ 'hide-comments': optionHideComments.checked });
+    });
+    chrome.storage.sync.get({ 'hide-comments': false }, (data) => {
+        optionHideComments.checked = data['hide-comments'];
+    });
+}
