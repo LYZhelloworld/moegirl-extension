b7678f59a970d87f492415ab3e12d3ed29f4800b
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2021-01-08 11:10:12 +0800

    Add i18n

--- popup/index.js
+++ popup/index.js
@@ -1,5 +1,10 @@
 'use strict';
 {
+    // i18n
+    document.querySelectorAll("[data-i18n]").forEach(element => {
+        element.textContent = chrome.i18n.getMessage(element.getAttribute("data-i18n"));
+    });
+
     let optionHideComments = document.getElementById('option-hide-comments');
     optionHideComments.addEventListener('click', () => {
         chrome.storage.sync.set({ 'hide-comments': optionHideComments.checked });
